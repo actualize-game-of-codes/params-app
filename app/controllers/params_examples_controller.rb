374900b057661657e8d69cf778631388af215adc
Peter Jang <peter.x.jang@gmail.com>
2017-07-06 18:38:21 -0500

    Add basic query params example

--- app/controllers/params_examples_controller.rb
+++ app/controllers/params_examples_controller.rb
@@ -1,2 +1,7 @@
 class ParamsExamplesController < ApplicationController
+  def query_params_method
+    @message1 = params["purple_hippo"]
+    @message2 = params["x"]
+    render "query_params.html.erb"
+  end
 end
